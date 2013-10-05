adcc9e7bd91b3823577326e254dab4a8c3216d66
Jared <jared@photosandtext.com>
2013-10-05 12:34:15 -0500

    Added HTML5 location stuff. Yay for clean URLs.

--- pat2_site/patjs/app.js
+++ pat2_site/patjs/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('patjs', ['patjs.filters', 'patjs.directives','patSvc', 'ngCookies', 'blueimp.fileupload', 'ui.bootstrap']).
   config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $routeProvider.when('/', {templateUrl: 'partials/photos.html',  reloadOnSearch: false, controller: photoListCtrl});
     $routeProvider.when('/photo/:photoID', {templateUrl: 'partials/photos.html', controller: photoListCtrl});
     $routeProvider.when('/upload', {templateUrl: 'partials/upload.html', controller: photoUploadCtrl});
