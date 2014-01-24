cccbcbfd37ae992e4974fa56b9185e6e631b0b36
Jared <jared@photosandtext.com>
2014-01-23 22:55:02 -0500

    Added super basic photo and gallery editing.

--- photosandtext2/static/js/patjs/controllers.js
+++ photosandtext2/static/js/patjs/controllers.js
@@ -1,10 +1,46 @@
 'use strict';
 
-function photoListCtrl($scope, galleryClient, $route, $routeParams, $rootScope, $http, $location) {
+function photoListCtrl($scope, galleryClient, photoClient, $route, $routeParams, $rootScope, $http, $location, $timeout) {
     delete $http.defaults.headers.common['X-Requested-With'];
     $scope.gallery = galleryClient.get({galleryID: $routeParams.galleryID});
     $scope.galleryUpdate = function(gallery) {
-            galleryClient.update({galleryID:gallery.id}, gallery);
+        galleryClient.update({galleryID:gallery.id}, gallery, function() {
+            $scope.galleryMessage("Saved Gallery!")
+        }, function(data){
+            console.log("fail?");
+        });
+    };
+
+    $scope.photoEditInit = function(photo) {
+        $scope.photo = photoClient.get({photoID: photo.id});
+        $scope.photoEdit = true;
+    };
+
+    $scope.galleryMessage = function(message) {
+        $scope.galleryMessageStatus = true;
+        $scope.galleryMessageContent = message;
+        $timeout(function(){
+            $scope.galleryMessageStatus = false;
+            $scope.galleryMessageContent = null;
+        },3000);
+    };
+
+    $scope.photoMessage = function(message) {
+        $scope.photoMessageStatus = true;
+        $scope.photoMessageContent = message;
+        $timeout(function(){
+            $scope.photoMessageStatus = false;
+            $scope.photoMessageContent = null;
+        },3000);
+    };
+
+    $scope.photoSave = function(photo) {
+        photoClient.update({photoID:photo.id}, photo, function(data){
+            console.log("success");
+            $scope.photoMessage("Saved Photo")
+        }, function(data){
+            console.log("fail?");
+        });
     };
 
 }
