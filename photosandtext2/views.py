75fc08ff3c7418401f58b896e8f6ed059e492005
Jared <jared@photosandtext.com>
2014-03-24 19:50:30 -0400

    More security on API

--- photosandtext2/views.py
+++ photosandtext2/views.py
@@ -1,7 +1,7 @@
 from flask import request, redirect, url_for, render_template, send_from_directory, flash, jsonify
 from flask.ext.login import login_user, login_required, logout_user, current_user
 from flask.ext.restless import ProcessingException
-from sqlalchemy import func, desc
+from sqlalchemy import func
 from werkzeug import secure_filename
 from photosandtext2 import app, manager, login_manager
 from photosandtext2.models.photo import Photo, Crop, Gallery
@@ -91,7 +91,10 @@ def edit_crops_for_photo_api(result, **kw):
     return result
 
 def remove_crops_for_photo_api(data, **kw):
-    del data["crops"]
+    try:
+        del data["crops"]
+    except:
+        pass
     pass
 
 def include_crops_for_gallery_api(result, **kw):
@@ -109,16 +112,19 @@ def include_crops_for_gallery_api(result, **kw):
 
 def remove_crops_for_gallery_api(data, **kw):
     newPhotos = []
-    for photo in data["photos"]:
-        del photo["crops"]
-        newPhotos.append(photo)
-    del data["photos"]
-    data["photos"] = newPhotos
+    try:
+        for photo in data["photos"]:
+            del photo["crops"]
+            newPhotos.append(photo)
+        del data["photos"]
+        data["photos"] = newPhotos
+    except:
+        pass
     pass
 
 def auth_func(**kw):
     if not current_user.is_authenticated():
-        raise ProcessingException(description='Not Authorized', code=401)
+        raise ProcessingException(message="You must be authorized to make this request", status_code=401)
 
 #Dont know if this counts as cheating or not.. kind of brute forcing file uploads into the API
 @app.route('/api/v1/galleries/<int:gallery_id>/photos', methods=['POST'])
@@ -145,7 +151,11 @@ manager.create_api(
     collection_name='photos',
     results_per_page=20,
     preprocessors={
-        'PUT_SINGLE':[remove_crops_for_photo_api, auth_func]
+        'PUT_SINGLE':[remove_crops_for_photo_api, auth_func],
+        'POST':[auth_func],
+        'PUT':[auth_func],
+        'DELETE':[auth_func],
+        'PATCH':[auth_func]
     },
     postprocessors={
         'GET_SINGLE':[include_url,edit_crops_for_photo_api]
@@ -159,7 +169,11 @@ manager.create_api(
     collection_name='galleries',
     results_per_page=20,
     preprocessors={
-        'PUT_SINGLE':[remove_crops_for_gallery_api, auth_func]
+        'PUT_SINGLE':[remove_crops_for_gallery_api, auth_func],
+        'POST':[auth_func],
+        'PUT':[auth_func],
+        'DELETE':[auth_func],
+        'PATCH':[auth_func]
     },
     postprocessors={
         'GET_SINGLE':[include_crops_for_gallery_api]
