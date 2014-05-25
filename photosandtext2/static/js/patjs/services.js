7c4dd7c2f019e9fabdba25afbcc9408b7a614929
Jared <jared@photosandtext.com>
2014-05-25 09:37:22 -0400

    No need to use full URL for angular

--- photosandtext2/static/js/patjs/services.js
+++ photosandtext2/static/js/patjs/services.js
@@ -4,13 +4,13 @@
 
 angular.module('patSvc', ['ngResource']).
     factory('photoClient', function($resource){
-        return $resource('http://127.0.0.1:port/api/v1/photos/:photoID/', {port:':5000', photoID:'@id'}, {
+        return $resource('/api/v1/photos/:photoID/', {photoID:'@id'}, {
             common: {withCredentials: true, crossDomain: true},
             update: {method:'PUT'}
             });
         }).
     factory('galleryClient', function($resource, $routeParams){
-        return $resource('http://127.0.0.1:port/api/v1/galleries/:galleryID/', {port: ':5000', galleryID:'@id'},{
+        return $resource('/api/v1/galleries/:galleryID/', {galleryID:'@id'},{
             common: {withCredentials: true, crossDomain: true},
             update: {method:'PUT'}
         });
