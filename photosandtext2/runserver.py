67feb82cd74b325d82abb21a4f8dfee6a2916e68
Jared <jared@photosandtext.com>
2014-07-19 11:02:57 -0400

    testing logging

--- photosandtext2/runserver.py
+++ photosandtext2/runserver.py
@@ -7,5 +7,5 @@ from photosandtext2 import views
 if __name__ == "__main__":
     if not app.debug:
         import logging
-        logging.basicConfig(filename='pat2.log',level=logging.DEBUG)
+        logging.basicConfig(filename='..\pat2.log',level=logging.DEBUG)
     app.run()
